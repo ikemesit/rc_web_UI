fc0342812faaffa019d5a0c382236612730ad944
ikemesit <ikemesit@gmail.com>
2018-10-01 20:32:11 +0800

    feat/initial-landing-page-design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { AppRoutesModule } from './app.routes';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    SharedModule,
+    AppRoutesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
